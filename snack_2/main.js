479fd28c535586ae6ddb9a544ed881ef1bd26d48
MicheleTodeschini <micheletodeschini01@gmail.com>
2026-03-04 17:01:08 +0100

    solved es 2

---
+++ snack_2/main.js
@@ -0,0 +1,15 @@
+console.log('funzia');
+
+/* Crea una arrow function che calcola il quadrato di un numero.
+Definisci una funzione chiamata quadrato che accetta un numero e restituisce il suo quadrato in una sola riga. */
+
+//arrow function
+const num = (num) => num * num
+console.log(num(6));
+
+//funzione dichiarativa
+function quadrato(num) {
+    return num * num
+}
+
+console.log(quadrato(4));
