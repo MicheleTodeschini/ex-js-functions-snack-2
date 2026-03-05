84a52199a1bbf300f7eaeb83b5a4a69d9a8db47e
MicheleTodeschini <micheletodeschini01@gmail.com>
2026-03-05 12:08:32 +0100

    inverted line 14 with 15 and commented away line 8 for es 8

--- snack_8/main.js
+++ snack_8/main.js
@@ -5,14 +5,14 @@ console.log('funzia');
 Scrivi una funzione contoAllaRovescia che accetta un numero n e stampa il conto alla rovescia da n a 0, con un intervallo di 1 secondo tra ogni numero. Quando arriva a 0, stampa "Tempo scaduto!" e interrompe il timer. */
 
 function contoAllaRovescia(num) {
-    console.log(num);
+    //console.log(num);
 
 
 
     setTimeout(() => {
         let numero = setInterval(() => {
-            num = num - 1
             console.log(num);
+            num = num - 1
             if (num === 0) {
                 clearInterval(numero)
                 console.log('tempo scaduto');
